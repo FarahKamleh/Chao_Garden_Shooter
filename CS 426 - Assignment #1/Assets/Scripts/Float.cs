b602f9a43a2e1256bc485b2a7b26f3c9a58beec5
FarahKamleh <fkamleh8800@gmail.com>
2024-01-23 04:59:43 -0600

    Floating Attempt
    Omochao floats up and down, but his position is incorrect.

--- CS 426 - Assignment #1/Assets/Scripts/Float.cs
+++ CS 426 - Assignment #1/Assets/Scripts/Float.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* note: referenced a thread in the Unity forum to do this */
+
 public class Float : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // set the speed
+    float speed = 1f;
+
+    // determine how high the object goes
+    float height = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // maintain original position
+        Vector3 pos = transform.position;
+
+        // calculate new y
+        float newY = Mathf.Sin(Time.time * speed);
+
+        // change the position with the newly calculated y
+        transform.position = new Vector3(pos.x, newY, pos.z) * height;
     }
 }
